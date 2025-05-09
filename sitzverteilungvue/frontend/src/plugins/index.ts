0499cd346e2314f029dbca53b47ef22b043c0dff
devtobi <28538704+devtobi@users.noreply.github.com>
2025-05-09 07:57:36 +0200

    Copied over refarch-templates

--- sitzverteilungvue/frontend/src/plugins/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from "vue";
-
-import router from "../router";
-import pinia from "./pinia";
-import vuetify from "./vuetify";
-
-export function registerPlugins(app: App) {
-  app.use(vuetify).use(router).use(pinia);
-}
