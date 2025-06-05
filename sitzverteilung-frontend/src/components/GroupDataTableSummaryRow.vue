74213a55dc3fe1313a0451759d1829ebd5c24071
devtobi <28538704+devtobi@users.noreply.github.com>
2025-06-05 17:48:21 +0200

    :sparkles: Added more summary data and validation to summary row

--- sitzverteilung-frontend/src/components/GroupDataTableSummaryRow.vue
+++ sitzverteilung-frontend/src/components/GroupDataTableSummaryRow.vue
@@ -5,14 +5,27 @@
       <p class="font-weight-bold">
         Gesamtanzahl: {{ amountOfGroups
         }}<span :class="{ 'text-red': tooManyGroups }">
-          (max. {{ maxGroups }})</span
+          (max. {{ expectedSeats }})</span
         >
       </p>
     </td>
     <td>
-      <p class="font-weight-bold">Gesamtanzahl: {{ groupSeatsSum }}</p>
+      <p class="font-weight-bold">
+        Gesamtanzahl: {{ totalSeats }} von {{ expectedSeats }}
+        <span
+          v-if="seatsTooHigh"
+          class="text-red"
+          >(überschritten)</span
+        ><span
+          v-else-if="seatsTooLow"
+          class="text-red"
+          >(fehlend)</span
+        >
+      </p>
     </td>
-    <td />
+    <td>
+      <p class="font-weight-bold">Gesamtanzahl: {{ totalVotes }}</p>
+    </td>
     <td />
   </tr>
 </template>
@@ -24,16 +37,24 @@ import { computed } from "vue";
 
 const props = defineProps<{
   groups: Group[];
-  maxGroups: number;
+  expectedSeats: number;
 }>();
 
 const amountOfGroups = computed(() => props.groups.length);
-const tooManyGroups = computed(() => amountOfGroups.value > props.maxGroups);
+const tooManyGroups = computed(
+  () => amountOfGroups.value > props.expectedSeats
+);
+const seatsTooHigh = computed(() => totalSeats.value > props.expectedSeats);
+const seatsTooLow = computed(() => totalSeats.value < props.expectedSeats);
 
-const groupSeatsSum = computed(() => {
+const totalSeats = computed(() => {
   return props.groups.reduce(
-    (sum, group) => sum + (group.committeeSeats ?? 0),
+    (sum, group) => sum + (group.committeeSeats ? group.committeeSeats : 0),
     0
   );
+});
+
+const totalVotes = computed(() => {
+  return props.groups.reduce((sum, group) => sum + (group.votes ?? 0), 0);
 });
 </script>
