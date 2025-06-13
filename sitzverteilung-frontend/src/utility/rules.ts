a21c6dac94c9e7ed54284b0cee8bab1ae32bf111
devtobi <28538704+devtobi@users.noreply.github.com>
2025-06-13 12:45:43 +0200

    :fire: Removed no longer required rules

--- sitzverteilung-frontend/src/utility/rules.ts
+++ sitzverteilung-frontend/src/utility/rules.ts
@@ -2,12 +2,6 @@ export const FieldValidationRules = {
   Required: (value: string) =>
     (value !== null && value !== undefined && value !== "") ||
     "Das Feld ist ein Pflichtfeld.",
-  Integer: (value: number) =>
-    Number.isInteger(value) || "Das Feld muss eine Ganzzahl enthalten.",
-  LargerThan: (num: number) => (value: number) =>
-    value > num || `Der Wert muss größer als ${num} sein.`,
-  LowerOrEqualThan: (num: number) => (value: number) =>
-    value <= num || `Der Wert darf maximal ${num} sein.`,
   IsUnique: (arr: string[]) => (value: string) =>
     arr.filter((val) => val === value).length < 2 ||
     "Es gibt andere Einträge mit identischem Wert.",
