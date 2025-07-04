a444a7552f064ea13aac1919fb36236f33cf8d1a
devtobi <28538704+devtobi@users.noreply.github.com>
2025-07-04 15:31:55 +0200

    :card_file_box: Added dummy test data

--- sitzverteilung-frontend/src/stores/basedata.ts
+++ sitzverteilung-frontend/src/stores/basedata.ts
@@ -3,8 +3,54 @@ import type { BaseData } from "@/types/BaseData";
 import { acceptHMRUpdate, defineStore } from "pinia";
 import { computed, ref } from "vue";
 
+import { UnionType } from "@/types/Union.ts";
+
 export const useBaseDataStore = defineStore("basedata", () => {
-  const internalBaseDatas = ref<BaseData[]>([]);
+  const internalBaseDatas = ref<BaseData[]>([
+    {
+      name: "Test Basedata",
+      committeeSize: 350,
+      groups: [
+        {
+          name: "Testpartei 1",
+          committeeSeats: 200,
+          votes: 100,
+        },
+        {
+          name: "Testpartei 2",
+          committeeSeats: 100,
+          votes: 50,
+        },
+        {
+          name: "Testpartei 3",
+          committeeSeats: 50,
+          votes: 10,
+        },
+      ],
+      unions: [
+        {
+          name: "Testfraktion",
+          unionType: UnionType.FRACTION,
+          groups: [0, 1, 2],
+        },
+        {
+          name: "Testfraktion 2",
+          unionType: UnionType.FRACTION,
+          groups: [1, 2],
+        },
+        {
+          name: "Testausschuss 1",
+          unionType: UnionType.COMMITTEE,
+          groups: [0, 2],
+        },
+        {
+          name: "Testausschuss 2",
+          unionType: UnionType.COMMITTEE,
+          groups: [1, 2],
+        },
+      ],
+    },
+  ]);
   const baseDatas = computed(() => internalBaseDatas.value);
 
   function addBaseData(baseData: BaseData) {
