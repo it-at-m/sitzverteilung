10d96b97dad4f9d3b2a8edc22f20e370f9e0aa10
GerhardPx <gerhard.zollner@muenchen.de>
2025-07-11 11:18:40 +0200

    add info dialog component and info for base dialog

---
+++ sitzverteilung-frontend/src/components/common/InfoDialog.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-dialog
+      :model-value="modelValue"
+      persistent
+      width="800"
+  >
+    <v-card>
+      <v-card-text class="dialog-text">
+        <slot name="dialog-text" />
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+            id="infodialog-btn-back"
+            variant="flat"
+            @click="back"
+        >
+          {{ backText }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+const {
+  backText = "Zurück zur Seite",
+} = defineProps<{
+  modelValue: boolean;
+  backText?: string;
+  dialogTitle: string;
+}>();
+
+const emit = defineEmits<{
+  back: [];
+}>();
+
+function back(): void {
+  emit("back");
+}
+</script>
--- sitzverteilung-frontend/src/views/DataView.vue
+++ sitzverteilung-frontend/src/views/DataView.vue
@@ -19,9 +19,47 @@
       @no="hideDeleteConfirmation"
       @yes="deleteSelectedBaseData"
     />
-    <v-row>
-      <v-col>
-        <h1>Verwaltung der Basisdaten</h1>
+    <info-dialog
+        :model-value="isInformationDialogShown"
+        dialog-title=""
+        @back="hideInformationDialog"
+    >
+      <template #dialog-text>
+        <div class="dialog-text">
+          <h2>Informationen zur Basisdatenübersicht</h2>
+        <p>In dieser Ansicht wird die Verwaltung der Basisdaten geregelt, bestehend aus:</p>
+        <ul>
+          <li>Verwaltung der Basisdatenübersicht</li>
+          <li>Parteien und Gruppierungen</li>
+          <li>Fraktionen und Ausschüsse</li>
+        </ul>
+        <p>Bei der Verwaltung der Basisdatenübersicht lassen sich bereits angelegte Basisdaten durch ein Drop-Down-Menü in die Übersicht übernehmen. Eingefügte Basisdaten lassen sich hier anlegen, löschen und über einen Link teilen.</p>
+        <p>Ein Anlegen neuer Daten ist nur dann möglich, wenn:</p>
+        <ul>
+          <li>Der Name des Basisdatensatzes und die Größe des Hauptorgans eingegeben wurden.</li>
+          <li>Mindestens eine Partei vorhanden ist.</li>
+        </ul>
+        <p>Der Name ist auf 50 Zeichen beschränkt, die Größe des Hauptorgans auf 999 Sitze.</p>
+        <p>Bei dem Namen der Parteien und Gruppierungen gilt ebenfalls ein Limit von 50 Zeichen. Zusätzlich bietet die Tabelle an, die Anzahl der Sitze und der Stimmen zu erfassen.</p>
+        <p>Bevor der Basisdatensatz angelegt werden kann, muss die Anzahl der Sitze aller Parteien addiert mit der Größe des Hauptorgans übereinstimmen. Gelöscht werden können die Parteien und Gruppierungen:</p>
+        <ul>
+          <li>Entweder einzeln in der jeweiligen Zeile.</li>
+          <li>Oder indem mehrere ausgewählt werden und auf Zeile bzw. Zeilen löschen geklickt wird.</li>
+        </ul>
+        <p>Fraktionen und Ausschüsse werden separat unter Parteien/Gruppierungen angezeigt. Sofern mindestens zwei Parteien oder Gruppierungen ausgewählt wurden, lassen diese sich innerhalb der Gruppierungstabelle in eine Fraktion oder einen Ausschuss zusammenfassen.</p>
+        <p>Jede Partei/Gruppierung kann nur Teil einer Fraktion bzw. eines Ausschusses sein. Es ist möglich, innerhalb der Fraktions-/Ausschusstabelle Parteien oder Gruppierungen wieder zu entfernen. Diese lassen sich allerdings nicht wieder hinzufügen, es sei denn, es wird eine neue Fraktion/ein neuer Ausschuss angelegt.</p>
+        </div>
+      </template>
+    </info-dialog>
+    <v-row align="center" class="d-flex">
+      <v-col class="d-flex align-center">
+        <h1 class="mr-2">Verwaltung der Basisdaten</h1>
+        <v-icon
+            @click="showInformationDialog"
+            :icon="mdiInformation"
+            class="mx-1"
+            size="x-large"
+        />
       </v-col>
     </v-row>
     <v-toolbar class="my-6 py-2 px-3 bg-primary">
@@ -88,7 +126,7 @@
 <script setup lang="ts">
 import type { BaseData } from "@/types/BaseData";
 
-import { mdiContentSave, mdiDelete, mdiShare } from "@mdi/js";
+import {mdiContentSave, mdiDelete, mdiInformation, mdiShare} from "@mdi/js";
 import { useClipboard } from "@vueuse/core";
 import { computed, nextTick, ref, useTemplateRef, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
@@ -105,6 +143,7 @@ import {
   writeUrlParamToObject,
 } from "@/utility/urlEncoder.ts";
 import { LimitConfiguration } from "@/utility/validation.ts";
+import InfoDialog from "@/components/common/InfoDialog.vue";
 
 const store = useBaseDataStore();
 const snackbar = useSnackbarStore();
@@ -299,4 +338,30 @@ function isValidBaseData(x: any): x is BaseData {
   );
 }
 /* eslint-enable @typescript-eslint/no-explicit-any */
+
+const isInformationDialogShown = ref(false);
+function showInformationDialog() {
+  isInformationDialogShown.value = true;
+}
+function hideInformationDialog() {
+  isInformationDialogShown.value = false;
+}
 </script>
+
+<style scoped>
+.dialog-text h2 {
+  margin-bottom: 0.5em; /* Abstand der Überschrift nach unten */
+}
+
+.dialog-text {
+  padding: 20px; /* Padding um den gesamten Text*/
+}
+
+.dialog-text ul {
+  padding-left: 20px; /* Einrückung der Liste */
+}
+
+.dialog-text p {
+  margin-bottom: 1em; /* Abstand zwischen Absätzen nach unten */
+}
+</style>
