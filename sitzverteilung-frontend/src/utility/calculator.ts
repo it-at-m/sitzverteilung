1f8e76089153f6e87cad512f5540b4e2e5bded31
devtobi <28538704+devtobi@users.noreply.github.com>
2025-08-01 15:29:07 +0200

    :rotating_light: Lint

--- sitzverteilung-frontend/src/utility/calculator.ts
+++ sitzverteilung-frontend/src/utility/calculator.ts
@@ -3,26 +3,29 @@ import type { CalculationGroupRatio } from "@/types/calculation/internal/Calcula
 import type { CalculationSeatDistribution } from "@/types/calculation/internal/CalculationSeatDistribution.ts";
 import type { CalculationSeatOrder } from "@/types/calculation/internal/CalculationSeatOrder.ts";
 
-function dHondt(calculationGroups: CalculationGroup[], committeeSize: number) {
+export function dHondt(
+  calculationGroups: CalculationGroup[],
+  committeeSize: number
+) {
   const seatDistribution: CalculationSeatDistribution = {};
   const seatOrder: CalculationSeatOrder = [];
 
   // initialize distributions with 0 seats for every group
   calculationGroups.forEach((group) => (seatDistribution[group.name] = 0));
 
-  // calculate ratios using increasing divisors
+  // calculate ratios using increasing divisors, maybe change iteration?
   const ratios: CalculationGroupRatio[] = [];
   calculationGroups.forEach((group) => {
     for (let i = 1; i <= committeeSize; i++) {
       ratios.push({
         groupName: group.name,
-        value: group.assignedSeats / i,
+        value: group.seatsOrVotes / i,
       });
     }
   });
 
   // Calculate relevant ratios and drop the others
-  ratios.sort((a, b) => a.value - b.value);
+  ratios.sort((a, b) => b.value - a.value);
   const topRatios = ratios.slice(0, committeeSize);
 
   // Calculate seat distribution and order
