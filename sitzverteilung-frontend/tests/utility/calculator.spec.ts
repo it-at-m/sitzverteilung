cb61df9ea9c8deebf94b2b024b5b73ce21f17bbb
devtobi <28538704+devtobi@users.noreply.github.com>
2025-08-06 14:37:01 +0200

    :recycle: Refactored test data into own methods

--- sitzverteilung-frontend/tests/utility/calculator.spec.ts
+++ sitzverteilung-frontend/tests/utility/calculator.spec.ts
@@ -3,79 +3,151 @@ import { describe, expect, test } from "vitest";
 import { CalculationGroup } from "../../src/types/calculation/internal/CalculationGroup";
 import { CalculationSeatDistribution } from "../../src/types/calculation/internal/CalculationSeatDistribution";
 import { CalculationSeatOrder } from "../../src/types/calculation/internal/CalculationSeatOrder";
+import { CalculationStale } from "../../src/types/calculation/internal/CalculationStale";
 import { dHondt } from "../../src/utility/calculator";
+import { CalculationGroupRatio } from "../../src/types/calculation/internal/CalculationGroupRatio";
+
+interface CalculationTestData {
+  given: {
+    committeeSize: number;
+    groups: CalculationGroup[];
+  },
+  expected: {
+    distribution: CalculationSeatDistribution,
+    order: CalculationSeatOrder,
+    stale: CalculationStale
+  }
+}
 
 describe("Calculator tests", () => {
   test("dHondt 1 no stale", () => {
-    // Data from https://www.bundeswahlleiterin.de/service/glossar/d/d-hondtsche-sitzverteilung.html
     // given
-    const committeeSize = 8;
-    const groups: CalculationGroup[] = [
-      {
-        name: "Partei A",
-        seatsOrVotes: 10_000,
-      },
-      {
-        name: "Partei B",
-        seatsOrVotes: 6_000,
-      },
-      {
-        name: "Partei C",
-        seatsOrVotes: 1_500,
-      },
-    ];
-    const expectedDistribution: CalculationSeatDistribution = {
-      "Partei A": 5,
-      "Partei B": 3,
-      "Partei C": 0,
-    };
-    const expectedOrder: CalculationSeatOrder = [
-      {
-        groupName: "Partei A",
-        value: 10_000.000,
-      },
-      {
-        groupName: "Partei B",
-        value: 6_000.000,
-      },
-      {
-        groupName: "Partei A",
-        value: 5_000.000,
-      },
-      {
-        groupName: "Partei A",
-        value: 3_333.333,
-      },
-      {
-        groupName: "Partei B",
-        value: 3_000.000,
-      },
-      {
-        groupName: "Partei A",
-        value: 2_500.000,
-      },
-      {
-        groupName: "Partei A",
-        value: 2_000.000,
-      },
-      {
-        groupName: "Partei B",
-        value: 2_000.000,
-      },
-    ];
+    const data = getDHondtTestDataNoStale1();
 
     // when
-    const result = dHondt(groups, committeeSize);
+    const result = dHondt(data.given.groups, data.given.committeeSize);
+    const order = result.order.map((order: CalculationGroupRatio) => {
+      return {
+        groupName: order.groupName,
+        value: Number(order.value.toFixed(3))
+      }
+    })
 
     // then
-    expect(result.distribution).toEqual(expectedDistribution);
-    expect(result.order).toEqual(expectedOrder);
+    expect(result.distribution).toEqual(data.expected.distribution);
+    expect(order).toEqual(data.expected.order);
+    expect(result.stale).toEqual(data.expected.stale);
   });
 
   test("dHondt 2 no stale", () => {
-      // given
-      const committeeSize = 22;
-      const groups: CalculationGroup[] = [
+    // given
+    const data = getDHondtTestDataNoStale2();
+
+    // when
+    const result = dHondt(data.given.groups, data.given.committeeSize);
+    const order = result.order.map((order: CalculationGroupRatio) => {
+      return {
+        groupName: order.groupName,
+        value: Number(order.value.toFixed(3))
+      }
+    })
+
+    // then
+    expect(result.distribution).toEqual(data.expected.distribution);
+    expect(order).toEqual(data.expected.order);
+    expect(result.stale).toEqual(data.expected.stale);
+  });
+
+  test("dHondt stale", () => {
+    // given
+    const data = getDHondtTestDataStale()
+
+    // when
+    const result = dHondt(data.given.groups, data.given.committeeSize);
+    const order = result.order.map((order: CalculationGroupRatio) => {
+      return {
+        groupName: order.groupName,
+        value: Number(order.value.toFixed(3))
+      }
+    })
+
+    // then
+    expect(result.distribution).toEqual(data.expected.distribution);
+    expect(order).toEqual(data.expected.order);
+    expect(result.stale).toEqual(data.expected.stale);
+  });
+});
+
+// Data from https://www.bundeswahlleiterin.de/service/glossar/d/d-hondtsche-sitzverteilung.html
+function getDHondtTestDataNoStale1(): CalculationTestData {
+  return {
+    given: {
+      committeeSize: 8,
+      groups: [
+        {
+          name: "Partei A",
+          seatsOrVotes: 10_000,
+        },
+        {
+          name: "Partei B",
+          seatsOrVotes: 6_000,
+        },
+        {
+          name: "Partei C",
+          seatsOrVotes: 1_500,
+        },
+      ]
+    },
+    expected: {
+      distribution: {
+        "Partei A": 5,
+        "Partei B": 3,
+        "Partei C": 0,
+      },
+      order: [
+        {
+          groupName: "Partei A",
+          value: 10_000.0,
+        },
+        {
+          groupName: "Partei B",
+          value: 6_000.0,
+        },
+        {
+          groupName: "Partei A",
+          value: 5_000.0,
+        },
+        {
+          groupName: "Partei A",
+          value: 3_333.333,
+        },
+        {
+          groupName: "Partei B",
+          value: 3_000.0,
+        },
+        {
+          groupName: "Partei A",
+          value: 2_500.0,
+        },
+        {
+          groupName: "Partei A",
+          value: 2_000.0,
+        },
+        {
+          groupName: "Partei B",
+          value: 2_000.0,
+        },
+      ],
+      stale: undefined
+    },
+  }
+}
+
+function getDHondtTestDataNoStale2(): CalculationTestData {
+  return {
+    given: {
+      committeeSize: 22,
+      groups: [
         {
           name: "Partei A",
           seatsOrVotes: 24,
@@ -103,9 +175,11 @@ describe("Calculator tests", () => {
         {
           name: "Partei G",
           seatsOrVotes: 3,
-        }
-      ];
-      const expectedDistribution: CalculationSeatDistribution = {
+        },
+      ]
+    },
+    expected: {
+      distribution: {
         "Partei A": 7,
         "Partei B": 6,
         "Partei C": 6,
@@ -113,39 +187,39 @@ describe("Calculator tests", () => {
         "Partei E": 1,
         "Partei F": 1,
         "Partei G": 0,
-      };
-      const expectedOrder: CalculationSeatOrder = [
+      },
+      order: [
         {
           groupName: "Partei A",
-          value: 24.000,
+          value: 24.0,
         },
         {
           groupName: "Partei B",
-          value: 20.000,
+          value: 20.0,
         },
         {
           groupName: "Partei C",
-          value: 19.000,
+          value: 19.0,
         },
         {
           groupName: "Partei A",
-          value: 12.000,
+          value: 12.0,
         },
         {
           groupName: "Partei B",
-          value: 10.000,
+          value: 10.0,
         },
         {
           groupName: "Partei C",
-          value: 9.500,
+          value: 9.5,
         },
         {
           groupName: "Partei A",
-          value: 8.000,
+          value: 8.0,
         },
         {
           groupName: "Partei B",
-          value: 6.666,
+          value: 6.667,
         },
         {
           groupName: "Partei C",
@@ -153,47 +227,47 @@ describe("Calculator tests", () => {
         },
         {
           groupName: "Partei A",
-          value: 6.000,
+          value: 6.0,
         },
         {
           groupName: "Partei D",
-          value: 6.000,
+          value: 6.0,
         },
         {
           groupName: "Partei B",
-          value: 5.000,
+          value: 5.0,
         },
         {
           groupName: "Partei A",
-          value: 4.800,
+          value: 4.8,
         },
         {
           groupName: "Partei C",
-          value: 4.750,
+          value: 4.75,
         },
         {
           groupName: "Partei A",
-          value: 4.000,
+          value: 4.0,
         },
         {
           groupName: "Partei B",
-          value: 4.000,
+          value: 4.0,
         },
         {
           groupName: "Partei E",
-          value: 4.000,
+          value: 4.0,
         },
         {
           groupName: "Partei F",
-          value: 4.000,
+          value: 4.0,
         },
         {
           groupName: "Partei C",
-          value: 3.800,
+          value: 3.8,
         },
         {
           groupName: "Partei A",
-          value: 3.428,
+          value: 3.429,
         },
         {
           groupName: "Partei B",
@@ -201,81 +275,56 @@ describe("Calculator tests", () => {
         },
         {
           groupName: "Partei C",
-          value: 3.166,
+          value: 3.167,
         },
-    ];
+      ],
+      stale: undefined
+    }
+  }
+}
 
-    // when
-    const result = dHondt(groups, committeeSize);
-
-    // then
-    expect(result.distribution).toEqual(expectedDistribution);
-    expect(result.order).toEqual(expectedOrder);
-  })
-
-  test("dHondt no stale", () => {
-    // Data from https://www.bundeswahlleiterin.de/service/glossar/d/d-hondtsche-sitzverteilung.html
-    // given
-    const committeeSize = 7;
-    const groups: CalculationGroup[] = [
-      {
-        name: "Partei A",
-        seatsOrVotes: 10_000,
+function getDHondtTestDataStale(): CalculationTestData {
+  return {
+    given: {
+      committeeSize: 7,
+      groups: getDHondtTestDataNoStale1().given.groups
+    },
+    expected: {
+      distribution: {
+        "Partei A": 4,
+        "Partei B": 2,
+        "Partei C": 0,
       },
-      {
-        name: "Partei B",
-        seatsOrVotes: 6_000,
-      },
-      {
-        name: "Partei C",
-        seatsOrVotes: 1_500,
-      },
-    ];
-    const expectedDistribution: CalculationSeatDistribution = {
-      "Partei A": 5,
-      "Partei B": 3,
-      "Partei C": 0,
-    };
-    const expectedOrder: CalculationSeatOrder = [
-      {
-        groupName: "Partei A",
-        value: 10_000.000,
-      },
-      {
-        groupName: "Partei B",
-        value: 6_000.000,
-      },
-      {
-        groupName: "Partei A",
-        value: 5_000.000,
-      },
-      {
-        groupName: "Partei A",
-        value: 3_333.333,
-      },
-      {
-        groupName: "Partei B",
-        value: 3_000.000,
-      },
-      {
-        groupName: "Partei A",
-        value: 2_500.000,
-      },
-      {
-        groupName: "Partei A",
-        value: 2_000.000,
-      },
-      {
-        groupName: "Partei B",
-        value: 2_000.000,
-      },
-    ];
-
-    // when
-    const result = dHondt(groups, committeeSize);
-
-    // then
-    expect(result.distribution).toEqual(expectedDistribution);
-    expect(result.order).toEqual(expectedOrder);
-  });
-});
+      order: [
+        {
+          groupName: "Partei A",
+          value: 10_000.0,
+        },
+        {
+          groupName: "Partei B",
+          value: 6_000.0,
+        },
+        {
+          groupName: "Partei A",
+          value: 5_000.0,
+        },
+        {
+          groupName: "Partei A",
+          value: 3_333.333,
+        },
+        {
+          groupName: "Partei B",
+          value: 3_000.0,
+        },
+        {
+          groupName: "Partei A",
+          value: 2_500.0,
+        },
+      ],
+      stale: {
+        groupNames: ["Partei A", "Partei B"],
+        amountSeats: 1,
+      }
+    }
+  }
+}
