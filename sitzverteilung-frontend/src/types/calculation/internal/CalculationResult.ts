f8426c4970c33c8fe596216d7b2ef168bbd8df6e
devtobi <28538704+devtobi@users.noreply.github.com>
2025-10-10 14:03:03 +0200

    :recycle: Added exposure of aggregated calculation group seats

--- sitzverteilung-frontend/src/types/calculation/internal/CalculationResult.ts
+++ sitzverteilung-frontend/src/types/calculation/internal/CalculationResult.ts
@@ -1,8 +1,10 @@
 import type { CalculationMethod } from "@/types/calculation/CalculationMethod.ts";
 import type { CalculationMethodResult } from "@/types/calculation/internal/CalculationMethodResult.ts";
 import type { CalculationProportions } from "@/types/calculation/internal/CalculationProportions.ts";
+import type { CalculationSeatDistribution } from "@/types/calculation/internal/CalculationSeatDistribution.ts";
 
 export interface CalculationResult {
+  seats: CalculationSeatDistribution;
   proportions: CalculationProportions;
   methods: Partial<Record<CalculationMethod, CalculationMethodResult>>;
 }
