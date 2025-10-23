f90c8bfb3da99e8f620f29de5e3e5af8410f5d69
GerhardPx <gerhard.zollner@muenchen.de>
2025-10-23 15:21:50 +0200

    prettier

--- sitzverteilung-frontend/src/views/CalculationView.vue
+++ sitzverteilung-frontend/src/views/CalculationView.vue
@@ -123,7 +123,11 @@ watch(
 
     if (expandTimer) clearTimeout(expandTimer);
     expandTimer = setTimeout(() => {
-      if (!isExpanded.value && !isValid.value && currentBaseData.value.groups.length < 2) {
+      if (
+        !isExpanded.value &&
+        !isValid.value &&
+        currentBaseData.value.groups.length < 2
+      ) {
         toggleExpansion();
       }
     }, 100);
