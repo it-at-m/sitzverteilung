6aa65dde3d9e91ef738af6cedb2ba73f472b46c7
Tobias Stadler <28538704+devtobi@users.noreply.github.com>
2025-10-30 16:52:10 +0100

    Feature: Added detail dialog (#212)
    Co-authored-by: GerhardPx <gerhard.zollner@muenchen.de>

---
+++ sitzverteilung-frontend/src/components/result/DetailDialog.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-dialog
+    v-model="isDialogShown"
+    width="auto"
+    scrollable
+  >
+    <v-card
+      :title="`Detaillierte Informationen zum ${calculationMethod}-Verfahren`"
+      :prepend-icon="mdiInformation"
+    >
+      <v-card-text>
+        <p>
+          Größe des Hauptorgans: <b>{{ committeeSize ?? "keine Angabe" }}</b>
+        </p>
+        <p>
+          Größe des Ausschusses: <b>{{ targetSize ?? "keine Angabe" }}</b>
+        </p>
+        <result-table
+          class="mt-2"
+          :calculation-result="calculationResult"
+          :method-to-display="calculationMethod"
+        />
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          text="Schließen"
+          @click="isDialogShown = false"
+        />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import type { CalculationMethod } from "@/types/calculation/CalculationMethod.ts";
+import type { CalculationResult } from "@/types/calculation/internal/CalculationResult.ts";
+
+import { mdiInformation } from "@mdi/js";
+
+import ResultTable from "@/components/result/ResultTable.vue";
+
+const isDialogShown = defineModel<boolean>({ required: true });
+defineProps<{
+  calculationMethod: CalculationMethod;
+  targetSize?: number;
+  committeeSize?: number;
+  calculationResult?: CalculationResult;
+}>();
+</script>
--- sitzverteilung-frontend/src/components/result/ResultTable.vue
+++ sitzverteilung-frontend/src/components/result/ResultTable.vue
@@ -1,132 +1,151 @@
 <template>
-  <v-dialog
-    v-model="dialog"
-    max-width="600px"
-  >
-    <v-card>
-      <v-card-title>{{ detailTitle }}</v-card-title>
-      <v-card-text>
-        <p>Detailinformationen {{ detailInfo }}</p>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn
-          variant="text"
-          @click="dialog = false"
-          text="Schließen"
-        />
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
-  <v-toolbar flat>
-    <v-toolbar-title>Detailansicht zu:</v-toolbar-title>
-    <v-btn
-      v-for="method in AVAILABLE_METHODS"
-      :key="method"
-      :text="method"
-      variant="outlined"
-      class="mx-2"
-      @click="goToDetail(method)"
-      :disabled="!mappedResult.length"
-    />
-  </v-toolbar>
-  <div class="result-table">
-    <v-data-table
-      :headers="headers"
-      :items="mappedResult"
-      hide-default-footer
-      no-filter
-      disable-sort
-      density="compact"
-      no-data-text=""
-      :items-per-page="-1"
-    >
-      <template
-        v-for="method in AVAILABLE_METHODS"
-        :key="method"
-        v-slot:[`item.${method}${ResultDataSuffix.validationSuffix}`]="{ item }"
+  <v-container class="px-0">
+    <div class="result-table">
+      <v-data-table
+        :headers="headers"
+        :items="mappedResult"
+        hide-default-footer
+        no-filter
+        disable-sort
+        density="compact"
+        no-data-text=""
+        :items-per-page="-1"
       >
-        <template v-if="!item[`${method}${ResultDataSuffix.validationSuffix}`]">
-          <v-tooltip>
-            <template v-slot:activator="{ props }">
-              <v-icon
-                :icon="mdiClose"
-                color="error"
-                v-bind="props"
-              />
-            </template>
-            <span style="white-space: pre-line">
-              {{ generateValidationText(item, method) }}
-            </span>
-          </v-tooltip>
-        </template>
-        <template v-else>
-          <v-icon
-            :icon="mdiCheck"
-            color="success"
-          />
-        </template>
-      </template>
-      <template
-        v-for="method in AVAILABLE_METHODS"
-        :key="method"
-        v-slot:[`item.${method}${ResultDataSuffix.staleSuffix}`]="{ item }"
-      >
-        <template v-if="item[`${method}${ResultDataSuffix.staleSuffix}`]">
-          <v-tooltip>
-            <template v-slot:activator="{ props }">
-              <v-icon
-                :icon="mdiHandBackRight"
-                v-bind="props"
-              />
-            </template>
-            <span style="white-space: pre-line">
-              {{ generateStaleText(item, method) }}
-            </span>
-          </v-tooltip>
-        </template>
-      </template>
-      <template
-        v-for="method in AVAILABLE_METHODS"
-        :key="method"
-        v-slot:[`header.${method}${ResultDataSuffix.validationSuffix}`]="{
-          column,
-        }"
-      >
-        <div
-          v-if="mappedResult.length"
-          class="d-flex flex-row align-center ga-2"
+        <template
+          v-for="method in methodsToDisplay"
+          :key="method"
+          v-slot:[`header.${method}Title`]="{ column }"
         >
-          <span
-            :class="{ 'bg-error': !isMethodValid(method) }"
-            class="px-2 py-1 rounded-sm"
+          <v-tooltip
+            text="Detaillierte Informationen anzeigen"
+            :disabled="!mappedResult.length"
+            location="top"
+            v-if="!methodToDisplay"
           >
-            {{ column.title }}
-          </span>
-          <v-icon
-            :icon="isMethodValid(method) ? mdiCheck : mdiClose"
-            :color="isMethodValid(method) ? 'success' : 'error'"
-          />
-        </div>
-      </template>
-    </v-data-table>
-  </div>
-  <v-row v-if="!mappedResult.length">
-    <v-col>
-      <v-alert
-        text="Keine Berechnung möglich, da Daten unvollständig oder nicht valide sind."
-        type="error"
-        variant="tonal"
-      />
-    </v-col>
-  </v-row>
+            <template v-slot:activator="{ props }">
+              <v-btn
+                v-bind="props"
+                :disabled="!mappedResult.length"
+                @click="emit('clickedCalculationMethod', method)"
+                :text="column.title"
+                class="my-3"
+                :prepend-icon="mdiInformation"
+                variant="flat"
+                color="primary"
+                block
+              />
+            </template>
+          </v-tooltip>
+          <template v-else>
+            <span> {{ column.title }}</span>
+          </template>
+        </template>
+        <template
+          v-for="method in methodsToDisplay"
+          :key="method"
+          v-slot:[`header.${method}${ResultDataSuffix.validationSuffix}`]="{
+            column,
+          }"
+        >
+          <div
+            v-if="mappedResult.length"
+            class="d-flex flex-row justify-center align-center ga-2"
+          >
+            <span
+              :class="{ 'bg-error': !isMethodValid(method) }"
+              class="px-2 py-1 rounded-sm"
+            >
+              {{ column.title }}
+            </span>
+            <v-icon
+              :icon="isMethodValid(method) ? mdiCheck : mdiClose"
+              :color="isMethodValid(method) ? 'success' : 'error'"
+            />
+          </div>
+        </template>
+        <template
+          v-for="method in methodsToDisplay"
+          :key="method"
+          v-slot:[`item.${method}${ResultDataSuffix.staleSuffix}`]="{ item }"
+        >
+          <template v-if="item[`${method}${ResultDataSuffix.staleSuffix}`]">
+            <v-tooltip v-if="!methodToDisplay">
+              <template v-slot:activator="{ props }">
+                <v-icon
+                  :icon="mdiHandBackRight"
+                  v-bind="props"
+                />
+              </template>
+              <span style="white-space: pre-line">
+                {{ generateStaleText(item, method) }}
+              </span>
+            </v-tooltip>
+            <template v-else>
+              <span style="white-space: pre-line">
+                {{ generateStaleText(item, method) }}
+              </span>
+            </template>
+          </template>
+        </template>
+        <template
+          v-for="method in methodsToDisplay"
+          :key="method"
+          v-slot:[`item.${method}${ResultDataSuffix.validationSuffix}`]="{
+            item,
+          }"
+        >
+          <template
+            v-if="!item[`${method}${ResultDataSuffix.validationSuffix}`]"
+          >
+            <v-tooltip v-if="!methodToDisplay">
+              <template v-slot:activator="{ props }">
+                <div class="d-flex flex-row justify-center">
+                  <v-icon
+                    :icon="mdiClose"
+                    color="error"
+                    v-bind="props"
+                  />
+                </div>
+              </template>
+              <span style="white-space: pre-line">
+                {{ generateValidationText(item, method) }}
+              </span>
+            </v-tooltip>
+            <template v-else>
+              <span style="white-space: pre-line">
+                {{ generateValidationText(item, method) }}
+              </span>
+            </template>
+          </template>
+          <template v-else>
+            <div class="d-flex flex-row justify-center">
+              <v-icon
+                :icon="mdiCheck"
+                color="success"
+              />
+            </div>
+          </template>
+        </template>
+      </v-data-table>
+    </div>
+    <v-row v-if="!mappedResult.length">
+      <v-col>
+        <v-alert
+          text="Keine Berechnung möglich, da Daten unvollständig oder nicht valide sind."
+          type="error"
+          variant="tonal"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
 import type { CalculationResult } from "@/types/calculation/internal/CalculationResult.ts";
 import type { ResultData } from "@/types/calculation/ui/ResultData.ts";
 
-import { mdiCheck, mdiClose, mdiHandBackRight } from "@mdi/js";
-import { computed, ref } from "vue";
+import { mdiCheck, mdiClose, mdiHandBackRight, mdiInformation } from "@mdi/js";
+import { computed } from "vue";
 
 import {
   AVAILABLE_METHODS,
@@ -135,13 +154,18 @@ import {
 import { ResultDataSuffix } from "@/types/calculation/ui/ResultDataSuffix.ts";
 import { mapCalculationResultToResultData } from "@/utility/resultMapping.ts";
 
-const props = defineProps<{
+const { calculationResult, methodToDisplay } = defineProps<{
   calculationResult?: CalculationResult;
+  methodToDisplay?: CalculationMethod;
 }>();
 
+const methodsToDisplay = computed(() =>
+  methodToDisplay ? [methodToDisplay] : AVAILABLE_METHODS
+);
+
 const mappedResult = computed<ResultData[]>(() =>
-  props.calculationResult
-    ? mapCalculationResultToResultData(props.calculationResult)
+  calculationResult
+    ? mapCalculationResultToResultData(calculationResult)
     : ([] as ResultData[])
 );
 
@@ -159,59 +183,43 @@ const headers = computed(() => [
       {
         title: "Name",
         key: "name",
-        width: 200,
       },
       {
         title: "Sitze",
         key: "seatsOrVotes",
-        width: 50,
       },
     ],
   },
-  {
-    title: "Ergebnisse",
-    children: AVAILABLE_METHODS.map((method) => ({
-      title: `${method}`,
-      children: [
-        {
-          title: "Sitze",
-          key: `${method}${ResultDataSuffix.seatsSuffix}`,
-          width: 50,
-        },
-        {
-          title: "Patt",
-          key: `${method}${ResultDataSuffix.staleSuffix}`,
-          width: 50,
-        },
-        {
-          title: "Zulässigkeit",
-          key: `${method}${ResultDataSuffix.validationSuffix}`,
-          width: 50,
-        },
-      ],
-    })),
-  },
+  ...methodsToDisplay.value.map((method) => ({
+    title: `${method}`,
+    key: `${method}Title`,
+    children: [
+      {
+        title: "Sitze",
+        key: `${method}${ResultDataSuffix.seatsSuffix}`,
+      },
+      {
+        title: "Patt",
+        key: `${method}${ResultDataSuffix.staleSuffix}`,
+        width: !methodToDisplay ? 50 : undefined,
+        align: !methodToDisplay ? "center" : "left",
+      },
+      {
+        title: "Zulässigkeit",
+        key: `${method}${ResultDataSuffix.validationSuffix}`,
+        width: !methodToDisplay ? 150 : undefined,
+      },
+    ],
+  })),
   {
     title: "Proporz",
     key: "proportion",
-    width: 50,
   },
 ]);
 
-const dialog = ref(false);
-const detailTitle = ref("");
-
-// Placeholder for future content, currently unused
-const detailInfo = ref<string>("");
-
-function goToDetail(selectedCalculationMethod: CalculationMethod) {
-  detailTitle.value = selectedCalculationMethod;
-  dialog.value = true;
-}
-
 function generateStaleText(item: ResultData, method: CalculationMethod) {
-  if (props.calculationResult !== undefined) {
-    const staleInfo = props.calculationResult.methods[method]?.stale;
+  if (calculationResult !== undefined) {
+    const staleInfo = calculationResult.methods[method]?.stale;
 
     if (staleInfo !== undefined && staleInfo.groupNames.includes(item.name)) {
       return `Anzahl betroffener Sitze: ${staleInfo.amountSeats}\nBeteiligt: ${staleInfo.groupNames.join(", ")}\nQuotient: ${staleInfo.ratio}`;
@@ -225,7 +233,7 @@ function generateValidationText(
   method: CalculationMethod
 ): string {
   const validationData =
-    props.calculationResult?.methods[method]?.validation?.[item.name];
+    calculationResult?.methods[method]?.validation?.[item.name];
   if (!validationData) return "Keine Validierungsdaten vorhanden";
 
   const reasons = [
@@ -240,6 +248,10 @@ function generateValidationText(
 
   return `Nicht zulässig wegen:\n- ${reasons.join("\n- ")}`;
 }
+
+const emit = defineEmits<{
+  clickedCalculationMethod: [calculationMethod: CalculationMethod];
+}>();
 </script>
 <style>
 .result-table .v-data-table td:not(:last-child),
