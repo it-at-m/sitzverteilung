aaf2a8f19966df9da2455561de8a39d931ad760a
devtobi <28538704+devtobi@users.noreply.github.com>
2025-10-30 11:56:57 +0100

    :rotating_light: Fix lint

--- sitzverteilung-frontend/src/components/result/SeatOrderTable.vue
+++ sitzverteilung-frontend/src/components/result/SeatOrderTable.vue
@@ -1,35 +1,39 @@
 <template>
   <div class="bordered-table">
     <v-data-table
-        :headers="headers"
-        :items="mappedSeatOrder"
-        no-filter
-        hide-default-footer
-        disable-sort
-        density="compact"
-        no-data-text="Es konnte kein Sitz eindeutig vergeben werden."
-        :items-per-page="-1"
+      :headers="headers"
+      :items="mappedSeatOrder"
+      no-filter
+      hide-default-footer
+      disable-sort
+      density="compact"
+      no-data-text="Es konnte kein Sitz eindeutig vergeben werden."
+      :items-per-page="-1"
     />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { CalculationSeatOrder } from "@/types/calculation/internal/CalculationSeatOrder.ts";
+
 import { computed } from "vue";
-import {formatVisiblePrecision} from "@/utility/numberFormatter.ts";
+
+import { formatVisiblePrecision } from "@/utility/numberFormatter.ts";
 
 const { seatOrder } = defineProps<{
   seatOrder?: CalculationSeatOrder;
 }>();
 
 const mappedSeatOrder = computed(() => {
-  const formattedRatios = formatVisiblePrecision(seatOrder.map(order => order.value));
+  const formattedRatios = formatVisiblePrecision(
+    seatOrder.map((order) => order.value)
+  );
   return seatOrder?.map((order, index) => {
     return {
       seatNumber: index + 1,
       name: order.groupName,
-      ratio: formattedRatios[index]
-    }
+      ratio: formattedRatios[index],
+    };
   });
 });
 
@@ -37,7 +41,7 @@ const headers = [
   {
     title: "Sitznummer",
     key: "seatNumber",
-    width: 100
+    width: 100,
   },
   {
     title: "Name",
