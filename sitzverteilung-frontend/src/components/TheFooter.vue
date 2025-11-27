b3e4619a97617723db16c709c01c74c65749478f
Tobias Stadler <28538704+devtobi@users.noreply.github.com>
2025-11-27 12:33:55 +0100

    Feature: Added impress (#249)

---
+++ sitzverteilung-frontend/src/components/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-footer
+    class="flex-column ga-1 py-4"
+    color="surface-light"
+  >
+    <v-btn
+      text="Impressum und Datenschutz"
+      variant="text"
+      to="/impress"
+    />
+
+    <v-divider
+      class="my-2"
+      thickness="2"
+      width="50"
+    />
+
+    <div>
+      {{ new Date().getFullYear() }} —
+      <strong>Landeshauptstadt München - it@M</strong>
+    </div>
+  </v-footer>
+</template>
